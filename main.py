3828a5be5c5e11d16c70283b67a133552367ab5d
powerfuldeff <powerfuldeff@gmail.com>
2018-03-20 09:12:52 +0300

    First site ready

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ RECIPIENT = ["maxlero@ya.ru", "powerfuldeff@gmail.com"]
 
 
 def send_email(subject, body, user=MAIL_USER, pwd=MAIL_PWD, recipient=RECIPIENT):
-    msg = MIMEText(body, 'plain', 'utf-8')
+    msg = MIMEText(body, 'html', 'utf-8')
     msg['Subject'] = Header(subject, 'utf-8')
     msg['From'] = user
     msg['To'] = ", ".join(recipient)
@@ -23,12 +23,10 @@ def send_email(subject, body, user=MAIL_USER, pwd=MAIL_PWD, recipient=RECIPIENT)
         server.starttls()
         server.login(user, pwd)
         server.sendmail(msg['From'], recipient, msg.as_string())
-        print('successfully sent the mail')
 
         return True
     except Exception as e:
         print(e)
-        print("failed to send mail")
     finally:
         server.quit()
 
@@ -48,23 +46,34 @@ if os.path.exists("caches"):
     for i in range(0, len(links)):
         if s.find(links[i]['cache'].encode()) == -1:
             filtered_links.append(links[i])
-        else:
-            print('true')
 
     f.close()
 
     message_content = ""
 
     for i in range(0, len(filtered_links)):
-        message_content += filtered_links[i]['heading'] + ': ' + filtered_links[i]['href'] + '\n\n'
+        message_content += '<li><a href="' + filtered_links[i]['href'] + '">' + filtered_links[i][
+            'heading'] + '</a></li><br>'
 
-    if send_email("Доступна возможность(и)", message_content):
-        f = open("caches", "w+")
+    if len(message_content) > 0:
+        message_content = '<ul>' + message_content + '</ul>'
 
-        for i in range(0, len(filtered_links)):
-            print(filtered_links[i]['cache'])
+        if send_email("Доступна возможность(и)", message_content):
+            i = 0
 
-        f.close()
+            f = open("caches", "a")
+
+            for i in range(0, len(filtered_links)):
+                f.write(filtered_links[i]['cache'] + '\n')
+                i += 1
+
+            f.close()
+
+            print('Successfully sent the mail with ' + str(i) + ' options')
+        else:
+            print("failed to send mail")
+    else:
+        print('Nothing to send')
 else:
     f = open("caches", "w+")
 
